4155c69c71d6b3a7139482d48b54af6c7212ff4c
Sebastán Holweger <80050935+SHolweger@users.noreply.github.com>
2023-04-13 18:00:41 -0600

    Add files via upload

--- Proyecto Fase1.cpp
+++ Proyecto Fase1.cpp
@@ -1,103 +1,103 @@
 #include <iostream>
+
 using namespace std;
-const char *nombre_archivo= "traductor.dat";
+const char * nombre_archivo = "traductor.dat";
 void Crear();
 void leer();
 void actualizar();
 void borrar();
-struct traducir{
-    char palabra[20];
-    char traduccion[20];
-    char funcionalidad[100];
+struct traducir {
+  char palabra[20];
+  char traduccion[20];
+  char funcionalidad[100];
 };
-void Crear(){
-    FILE*archivo=fopen(nombre_archivo,"a+b");
-    traducir Traducir;
-    char res;
-    do{
-        fflush(stdin);
-        cout<<"Ingrese Palabra: ";
-        fflush(stdin);
-        cin.getline(Traducir.palabra,20);
-        cout<<"Ingrese su respectiva traduccion: ";
-        cin.getline(Traducir.traduccion,20);
-        fflush(stdin);
-        cout<<"Ingrese su respectiva funcionalidad: ";
-        cin.getline(Traducir.funcionalidad,100);
+void Crear() {
+  FILE * archivo = fopen(nombre_archivo, "a+b");
+  traducir Traducir;
+  char res;
+  do {
+    fflush(stdin);
+    cout << "Ingrese Palabra: ";
+    fflush(stdin);
+    cin.getline(Traducir.palabra, 20);
+    cout << "Ingrese su respectiva traduccion: ";
+    cin.getline(Traducir.traduccion, 20);
+    fflush(stdin);
+    cout << "Ingrese su respectiva funcionalidad: ";
+    cin.getline(Traducir.funcionalidad, 100);
 
-
-        fwrite(&Traducir,sizeof(traducir),1,archivo);
-        cout<<"Desea ingresar otra palabra(S/N)";
-        cin>>res;
-        //system("cls");
-    }while(res=='s' || res=='S');
-    leer();
-    fclose(archivo);
-};
-void leer(){
+    fwrite( & Traducir, sizeof(traducir), 1, archivo);
+    cout << "Desea ingresar otra palabra(S/N)";
+    cin >> res;
     //system("cls");
-     FILE*archivo=fopen(nombre_archivo,"rb");
-     if(!archivo){
-           FILE*archivo=fopen(nombre_archivo,"w+b");
-     }
-     traducir Traducir;
-     int id=0;
-     fread(&Traducir,sizeof(traducir),1,archivo);
-     cout<<"=========================================="<<endl;
-     cout<<"ID"<<"|"<<"PALABRA"<<"|"<<"TRADUCCION"<<"|"<<"FUNCIONALIDAD"<<endl;
-     do{
-        cout<<id<<"|"<<Traducir.palabra<<"|"<<Traducir.traduccion<<"|"<<Traducir.funcionalidad<<endl;
-        fread(&Traducir,sizeof(traducir),1,archivo);
-        id+=1;
-     }while(feof(archivo)==0);
-     fclose(archivo);
+  } while (res == 's' || res == 'S');
+  leer();
+  fclose(archivo);
+};
+void leer() {
+  //system("cls");
+  FILE * archivo = fopen(nombre_archivo, "rb");
+  if (!archivo) {
+    FILE * archivo = fopen(nombre_archivo, "w+b");
+  }
+  traducir Traducir;
+  int id = 0;
+  fread( & Traducir, sizeof(traducir), 1, archivo);
+  cout << "==========================================" << endl;
+  cout << "ID" << "|" << "PALABRA" << "|" << "TRADUCCION" << "|" << "FUNCIONALIDAD" << endl;
+  do {
+    cout << id << "|" << Traducir.palabra << "|" << Traducir.traduccion << "|" << Traducir.funcionalidad << endl;
+    fread( & Traducir, sizeof(traducir), 1, archivo);
+    id += 1;
+  } while (feof(archivo) == 0);
+  fclose(archivo);
 };
 
-void actualizar(){
-    FILE* archivo = fopen(nombre_archivo,"r+b");
-	traducir Traducir;
-	int id=0;
-	cout<<"Ingrese el ID que desea Modificar:";
-	cin>>id;
-	fseek(archivo,id * sizeof(traducir),SEEK_SET);
-		fflush(stdin);
-		cout<<"Ingrese Palabra: ";
-		cin.getline(Traducir.palabra,20);
-		fflush(stdin);
-		cout<<"Ingrese Traduccion: ";
-		cin.getline(Traducir.traduccion,20);
-        fflush(stdin);
-		cout<<"Ingrese su respectiva funcionalidad: ";
-		cin.getline(Traducir.funcionalidad,100);
-		fwrite(&Traducir,sizeof(traducir),1,archivo);
-	fclose(archivo);
-	leer();
+void actualizar() {
+  FILE * archivo = fopen(nombre_archivo, "r+b");
+  traducir Traducir;
+  int id = 0;
+  cout << "Ingrese el ID que desea Modificar:";
+  cin >> id;
+  fseek(archivo, id * sizeof(traducir), SEEK_SET);
+  fflush(stdin);
+  cout << "Ingrese Palabra: ";
+  cin.getline(Traducir.palabra, 20);
+  fflush(stdin);
+  cout << "Ingrese Traduccion: ";
+  cin.getline(Traducir.traduccion, 20);
+  fflush(stdin);
+  cout << "Ingrese su respectiva funcionalidad: ";
+  cin.getline(Traducir.funcionalidad, 100);
+  fwrite( & Traducir, sizeof(traducir), 1, archivo);
+  fclose(archivo);
+  leer();
 
 };
-void borrar(){
-    const char *nombre_archivo_temp = "archivo_temp.dat";
-	FILE* archivo_temp = fopen(nombre_archivo_temp,"w+b");
-	FILE* archivo = fopen(nombre_archivo,"rb");
-	traducir Traducir;
-	int id=0,id_n=0;
-	cout<<"Ingrese el ID a eliminar:";
-	cin>>id;
-	while(fread(&Traducir,sizeof(traducir),1,archivo)){
-		if (id_n !=id ){
-			fwrite(&Traducir,sizeof(traducir),1,archivo_temp);
-		}
-		id_n++;
-	}
-	fclose(archivo);
-	fclose(archivo_temp);
+void borrar() {
+  const char * nombre_archivo_temp = "archivo_temp.dat";
+  FILE * archivo_temp = fopen(nombre_archivo_temp, "w+b");
+  FILE * archivo = fopen(nombre_archivo, "rb");
+  traducir Traducir;
+  int id = 0, id_n = 0;
+  cout << "Ingrese el ID a eliminar:";
+  cin >> id;
+  while (fread( & Traducir, sizeof(traducir), 1, archivo)) {
+    if (id_n != id) {
+      fwrite( & Traducir, sizeof(traducir), 1, archivo_temp);
+    }
+    id_n++;
+  }
+  fclose(archivo);
+  fclose(archivo_temp);
 
-	archivo_temp = fopen(nombre_archivo_temp,"rb");
-	archivo = fopen(nombre_archivo,"wb");
-	while(fread(&Traducir,sizeof(traducir),1,archivo_temp)){
-		fwrite(&Traducir,sizeof(traducir),1,archivo);
+  archivo_temp = fopen(nombre_archivo_temp, "rb");
+  archivo = fopen(nombre_archivo, "wb");
+  while (fread( & Traducir, sizeof(traducir), 1, archivo_temp)) {
+    fwrite( & Traducir, sizeof(traducir), 1, archivo);
 
-	}
-    fclose(archivo);
-	fclose(archivo_temp);
-	leer();
-};
+  }
+  fclose(archivo);
+  fclose(archivo_temp);
+  leer();
+};  
--- Proyecto Fase2.cpp
+++ Proyecto Fase2.cpp
@@ -4,69 +4,69 @@
 #include "Proyecto Fase1.cpp"
 #include <cstring>
 
-string reemplazar(const string& linea) {
-    char palabra[20];
-    char traduccion[20];
-    string resultado = linea;
-    FILE* archivo = fopen(nombre_archivo, "rb");
-    if (!archivo) {
-        archivo = fopen(nombre_archivo, "w+b");
+string reemplazar(const string & linea) {
+  char palabra[20];
+  char traduccion[20];
+  string resultado = linea;
+  FILE * archivo = fopen(nombre_archivo, "rb");
+  if (!archivo) {
+    archivo = fopen(nombre_archivo, "w+b");
+  }
+  traducir Traducir;
+  fread( & Traducir, sizeof(traducir), 1, archivo);
+  do {
+    //Copiar cadena
+    strncpy(palabra, Traducir.palabra, sizeof(palabra));
+    strncpy(traduccion, Traducir.traduccion, sizeof(traduccion));
+  
+    //tamaÃ±o cadena
+    size_t tamano = strlen(palabra);
+    size_t tamano_tra = strlen(traduccion);
+    //char a string
+    string mystring(palabra, tamano);
+    string mystring2(traduccion, tamano_tra);
+
+    //traducir palabras
+    size_t posicion = resultado.find(palabra);
+    while (posicion != string::npos) {
+      resultado.replace(posicion, tamano, traduccion);
+      posicion = resultado.find(palabra, posicion + tamano);
     }
-    traducir Traducir;
-    fread(&Traducir, sizeof(traducir), 1, archivo);
-    do {
-        //Copiar cadena
-        strncpy(palabra, Traducir.palabra, sizeof(palabra));
-        strncpy(traduccion, Traducir.traduccion, sizeof(traduccion));
 
-        //tamaño cadena
-        size_t tamano = strlen(palabra);
-        size_t tamano_tra = strlen(traduccion);
-        //char a string
-        string mystring(palabra, tamano);
-        string mystring2(traduccion, tamano_tra);
-
-        //traducir palabras
-        size_t posicion = resultado.find(palabra);
-        while (posicion != string::npos) {
-            resultado.replace(posicion, tamano, traduccion);
-            posicion = resultado.find(palabra, posicion + tamano);
-        }
-
-		//cambiar llaves por inicio y fin
-        posicion = resultado.find("{");
-        while (posicion != string::npos) {
-            resultado.replace(posicion, 1, " inicio");
-            posicion = resultado.find("{", posicion + 6);
-        }
-        posicion = resultado.find("}");
-        while (posicion != string::npos) {
-            resultado.replace(posicion, 1, " fin");
-            posicion = resultado.find("}", posicion + 3);
-        }
-        fread(&Traducir, sizeof(traducir), 1, archivo);
-    } while (!feof(archivo));
-    fclose(archivo);
-    return resultado;
+    //cambiar llaves por inicio y fin
+    posicion = resultado.find("{");
+    while (posicion != string::npos) {
+      resultado.replace(posicion, 1, " Inicio");
+      posicion = resultado.find("{", posicion + 6);
+    }
+    posicion = resultado.find("}");
+    while (posicion != string::npos) {
+      resultado.replace(posicion, 1, " Fin");
+      posicion = resultado.find("}", posicion + 3);
+    }
+    fread( & Traducir, sizeof(traducir), 1, archivo);
+  } while (!feof(archivo));
+  fclose(archivo);
+  return resultado;
 }
 int main() {
-    //Crear();
-    string texto;
-    string linea;
-    fflush(stdin);
-    cout << "Ingresa tu texto multilinea y presiona ENTER. Para terminar, ingresa una linea vacia:" << endl;
+  //Crear();
+  string texto;
+  string linea;
+  fflush(stdin);
+  cout << "Ingresa tu texto multilinea y presiona ENTER. Para terminar, ingresa una linea vacia:" << endl;
 
-    // Leer cada línea y agregarla al texto
-    while (getline(cin, linea)) {
-        if (linea.empty()) {
-            break;
-        }
-        texto += linea + "\n";
+  // Leer cada linea y agregarla al texto
+  while (getline(cin, linea)) {
+    if (linea.empty()) {
+      break;
     }
+    texto += linea + "\n";
+  }
 
-    // Imprimir el texto
-    cout << "Texto ingresado:\n" << texto << endl;
-    string texto_reemplazado;
-    texto_reemplazado = reemplazar(texto);
-    cout << texto_reemplazado << endl;
+  // Imprimir el texto
+  cout << "Texto ingresado:\n" << texto << endl;
+  string texto_reemplazado;
+  texto_reemplazado = reemplazar(texto);
+  cout << texto_reemplazado << endl;
 }
